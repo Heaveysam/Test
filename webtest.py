50fd74649dcd9152042e273b0eb8a3b4fdb0722b
Sam Heavey <heaveysam@github.com>
2019-03-26 17:50:48 +0000

    d

---
+++ webtest.py
@@ -0,0 +1,13 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/test/<x>')
+def test(x):
+	return '''<h1>the answer is not {}</h1>'''.format(x)
+
+if __name__ == "__main__":
+    # Setting debug to True enables debug output. This line should be
+    # removed before deploying a production app.
+    app.debug = True
+app.run(host = '0.0.0.0', port = 80)
